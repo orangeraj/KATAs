211860c88b5f77fb268af270ba8df00c049923a5
orangeie-admin <rajasmhatre93@gmail.com>
2023-08-08 22:46:21 +0530

    three sticks is tricky one

---
+++ Three sticks.py
@@ -0,0 +1,9 @@
+def maxlen(L1,L2):
+    # return maximal length
+    if L1 > L2:
+        return L1/3
+    else:
+        return L2/3
+
+#NOT THE CORRECT ANSWER, THIS KATA WENT BOUNCER
+print(maxlen(5,17))
